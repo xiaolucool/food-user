c6971e5d9e6581d1364b1d4533764a416a9307e5
xiaolucool <clueing@outlook.com>
2024-07-03 13:52:30 +0800

    移除vite-plugin-dist-zip插件并更新.gitignore
    移除了vite-plugin-dist-zip插件相关的代码，该插件用于生成zip文件。相应地，.gitignore中对dist目录的排除规则也进行了更新。

--- vite.config.ts
+++ vite.config.ts
@@ -7,9 +7,6 @@ import AutoImport from 'unplugin-auto-import/vite'
 import Components from 'unplugin-vue-components/vite'
 import { VantResolver } from '@vant/auto-import-resolver'
 
-import vitePluginZipDist from 'vite-plugin-dist-zip'
-import {name} from './package.json'
-
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
@@ -19,8 +16,7 @@ export default defineConfig({
     }),
     Components({
       resolvers: [VantResolver()],
-    }),
-    vitePluginZipDist({zipName: name, dayjsFormat: ''}),
+    })
   ],
   resolve: {
     alias: {
